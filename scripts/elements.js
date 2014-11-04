ebafb58b04a00771c354fe089ec1470a9a4d0678
Mary Racter <paracetamolboy@gmail.com>
2014-11-04 16:43:38 +0200

    added test background images. tweaked positioning/sizes and padding/margins. added appropriate menu dividers.

--- scripts/elements.js
+++ scripts/elements.js
@@ -5,18 +5,18 @@ function getHeader() {
 		<nav>\
 		<ul>\
 		<menu>\
-		  <li><a href="index.html">ABOUT</a></li>\
-		  <li><a href="alex.html">ALEX</a></li>\
-		  <li><a href="mary.html">MARY</a></li>\
-		  <li><a href="jamie.html">JAMIE</a></li>\
-		  <li><a href="">DEMO</a>\
+		  <li><a href="index.html" class="about">ABOUT</a></li>\
+		  <li><a href="alex.html" class="alex">ALEX</a></li>\
+		  <li><a href="mary.html" class="mary">MARY</a></li>\
+		  <li><a href="jamie.html" class="jamie">JAMIE</a></li>\
+		  <li><a href="" class="demo">DEMO</a>\
 		  	<ul>\
 		  		<li><a href="demo_alex.html">ALEX</a></li>\
 		  		<li><a href="demo_jamie.html">JAMIE</a></li>\
 		  		<li><a href="demo_mary.html">MARY</a></li>\
 		  	</ul>\
 		  </li>\
-		  <li><a href="download.html">DOWNLOAD</a></li>\
+		  <li><a href="download.html" class="download">DOWNLOAD</a></li>\
 		</menu>\
 		</ul>\
 		</nav>\
